8465dc1fd8f448bb919800d18b50b4494f163482
Zachary Hutto <zrhutto@me.com>
2017-05-22 14:16:27 -0400

    Remove MP3 output option and LAME dependency

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var isMacOrWin = require('os').type() == 'Darwin' || require('os').type().indexOf('Windows') > -1;
 var spawn = require('child_process').spawn
 var PassThrough = require('stream').PassThrough;
-var lame = require('lame');
 
 var ps = null;
 
@@ -9,24 +8,16 @@ var audio = new PassThrough;
 var info = new PassThrough;
 
 var start = function(options) {
-    options = options || {};
+    options = options || {},
+    device = options.device || 'plughw:1,0';
     
     if(ps == null) {
         ps = isMacOrWin
         ? spawn('sox', ['-d', '-t', 'dat', '-p'])
-        : spawn('arecord', ['-D', 'plughw:1,0', '-f', 'dat']);
+        : spawn('arecord', ['-D', device, '-f', 'dat']);
 
         if(options.mp3output === true) {
-            var encoder = new lame.Encoder( {
-                channels: 2,
-                bitDepth: 16,
-                sampleRate: 44100
-            });
-
-            ps.stdout.pipe(encoder);
-            encoder.pipe(audio);
-            ps.stderr.pipe(info);
-
+            console.err("MP3 output has been removed");
         } else {
             ps.stdout.pipe(audio);
             ps.stderr.pipe(info);
